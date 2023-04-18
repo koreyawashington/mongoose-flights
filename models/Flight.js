ddcc47abeb480175987913bc3620a2705519b0f3
koreyawashington <122809994+koreyawashington@users.noreply.github.com>
2023-04-18 19:52:33 -0400

    TypeError Flight find is not a function

--- models/Flight.js
+++ models/Flight.js
@@ -11,4 +11,22 @@ departs: {type: Date, required: true, default: Date.now}
 //mongoose model                      
 const Flight = mongoose.model('Flight', flightSchema);
 
-module.exports = Flight;
+// module.exports = Flight;
+
+// const mongoose = require('mongoose');
+
+// const flightSchema = new mongoose.Schema({
+//     airline: {
+//         type: String,
+//         enum: ['American', 'Southwest', 'United', 'Delta', 'Allegiant']
+//     },
+//     flightNo: {
+//         type: Number,
+//         min: 10,
+//         max: 9999
+//     },
+//     departs: Date
+// })
+
+// const Flight = mongoose.model('Flight', flightSchema)
+// module.exports = Flight;
--- server.js
+++ server.js
@@ -1,11 +1,65 @@
+require('dotenv').config(); // call and configure your dotenv package
 const express = require('express')
-const app = express()
+const connectToDB = require('./config/database')
+
+
 const port = 3000
+const app = express()
+
+const Flight = require('./models/Flight')
+
+// ==== Configuration
+app.set('view engine', 'jsx');
+app.engine('jsx', require('jsx-view-engine').createEngine());
+
+//* settling a middleware to run in our app
+app.use((req, res, next) => {
+    console.log(req.url);
+    next()
+})
+//* parses the data from the request
+app.use(express.urlencoded({extended: false}))
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/flights', (req, res) => {
+    Flight.find({}, (error, allFlights) => {
+        res.render('Index', {flights:allFlights})
+    })
+    })
+
+ //* Post method (accept data from the form)
+app.post('/flights', (req, res) => {
+    console.log(req.body);
+
+    Flight.create(req.body, (error, createdFlights) => {
+        // res.send(createdFlights)
+        res.redirect('/flights')
+    })
+})  
+
+app.get('/flights/new', (req, res) => {
+res.render('New')
+})
+
+
+
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  connectToDB()
 })
+//   mongoose.set('strictQuery', true)
+//   mongoose.connect(process.env.MONGO_URI,{
+//     useNewUrlPArser: true, 
+//     useUnifiedTopology: true,
+//   })
+//   mongoose.connection.once('open', () => {
+//     console.log('Connected to MongoDB!')
+//   })
+// })
